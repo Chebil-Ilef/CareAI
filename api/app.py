978ae0d62cedea0255cc03b1a464d84798c6e2ee
Lost Elf <ilef.chebil@insat.ucar.tn>
2024-05-31 15:07:10 +0000

    model alter

--- api/app.py
+++ api/app.py
@@ -22,7 +22,7 @@ cors = CORS(app)
 # Home route
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('Home/index.html')
 
 # Parkinson's Disease Prediction
 parkinson_model = pickle.load(open('./compressed_models/parkinson_disease/parkinson_classifier_model.pkl','rb'))
@@ -53,31 +53,31 @@ def parkinson():
 
 
 # Skin Cancer Prediction
-# skin_model = keras.models.load_model('./compressed_models/skin_cancer/skincancer.h5')
+skin_model = keras.models.load_model('./compressed_models/skin_cancer/ski.h5')
 
 @cross_origin()
 @app.route('/skin', methods=['GET', 'POST'])
 def skin():
-    # if request.method == 'POST':
-        # imagefile = request.files.get('skinImg')
+    if request.method == 'POST':
+        imagefile = request.files.get('skinImg')
 
-        # if imagefile:
-        #     # Read the image file
-        #     image_data = imagefile.read()
-        #     # Convert the image data into a PIL Image object
-        #     image = Image.open(io.BytesIO(image_data))
-        #     # Convert the PIL Image to OpenCV format (BGR)
-        #     opencvImage = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-        #     img = cv2.resize(opencvImage, (150, 150))
-        #     img = img.reshape(1, 150, 150, 3)
-        #     p = skin_model.predict(img)
-        #     p = np.argmax(p, axis=1)[0]
+        if imagefile:
+            # Read the image file
+            image_data = imagefile.read()
+            # Convert the image data into a PIL Image object
+            image = Image.open(io.BytesIO(image_data))
+            # Convert the PIL Image to OpenCV format (BGR)
+            opencvImage = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+            img = cv2.resize(opencvImage, (224, 224))
+            img = img.reshape(1, 224, 224, 3)
+            p = skin_model.predict(img)
+            p = np.argmax(p, axis=1)[0]
 
-        #     if p == 0:
-        #         result = 'Benign Tumor'
-        #     elif p == 1:
-        #         result = 'Maligant Tumor'
-        #     return jsonify({'result': result})
+            if p == 0:
+                result = 'Benign Tumor'
+            elif p == 1:
+                result = 'Maligant Tumor'
+            return jsonify({'result': result})
         
     return render_template('Skin/index.html')
    
